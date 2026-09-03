c0d44151f6941a82947b7474727c9a94a1436b72
sabbarbahija <bohija.sabbar11@gmail.com>
2026-09-03 17:58:30 +0200

    challenge 8

--- Day3/fonction/challenge8.js
+++ Day3/fonction/challenge8.js
@@ -1,4 +1,15 @@
 // Challenge 8 : Convertisseur Universel de Devises avec Callback
+  function formatMAD(val){
+    return val+"DH";
+}  
+
 function convertirMontant(montant,taux ,formatter){
+    let r= montant * taux;
     
+return formatMAD(r);
+
 }
+let prompt = require('prompt-sync')();
+let mot = convertirMontant(100,5,formatMAD);
+console.log(mot);
+
